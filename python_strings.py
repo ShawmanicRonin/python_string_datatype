1a03643222f14b49a227a9247ec7ebdbd257d462
Ronin Evans <41272060+ShawmanicRonin@users.noreply.github.com>
2024-09-26 18:08:58 -0600

    applied new code to learn from lines 1 to 39

--- python_strings.py
+++ python_strings.py
@@ -1,12 +1,12 @@
 # TODO Create Variables
 #   - Create the following variables
-#   - my_first_name
+my_first_name = "Ronin"
 #       -set this equal to your first name
-#   - my_last_name
+my_last_name = "Evans"
 #       -set this equal to your last name
-#   - my_year_of_birth
+my_year_of_birth = 1998
 #       -set this equal to your birth year (doesn't have to be real should be less then 100 yrs ago)
-#   - current_year
+current_year = 2024
 #       -set this equal to 2020
 
 
@@ -14,12 +14,19 @@
 
 # TODO String Indexing
 #   - Print the following items (one per line) (print using variables)
-#       - first name  
+#       - first name
+print(my_first_name)  
 #       - last name
+print(my_last_name)
 #       - first letter of your first name (use the +index)
+print(my_first_name[0])
 #       - second letter of your last name (use the -index)
+print(my_last_name[-4])
 #       - first two letter of your first name (use the +index)
+print(my_first_name[0:2])
 #       - second two letter of your last name (use the -index)
+print(my_last_name[-4:-2])
+#print(my_last_name[-4:-2]) press shift, alt, down arrow key to copy the above line to the next line
 
 
 
@@ -27,7 +34,9 @@
 #TODO Combining Strings
 #   - Print the following items (one per line) (print using variables)
 #       -first name and last name combined
+print(my_first_name+my_last_name)
 #       -first name six times
+print(my_first_name*6)
 
 
 
@@ -36,6 +45,7 @@
 # TODO Formatting Strings
 #   - Print the following items (one per line) (print using variables)
 #       - first name last name -was born in- year of birth
+
 #       - first name last name -was born in- year of birth. first name -enjoyed celebrating- current year
 
 
